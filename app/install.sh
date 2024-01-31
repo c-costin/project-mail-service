341bdb6058a768ce4d846d8befbb6f9bad08b815
c-costin <contact@costincadeau.fr>
2024-01-31 14:32:54 +0100

    build: create installation script, install Symfony project and create compose.yml

---
+++ app/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Creation symfony project
+composer create-project symfony/skeleton:"6.4.*" install
+
+# Move directory project
+mv ./install/* ./install/.* .
+rmdir ./install/
+
+# Require bundle env:DEV
+composer require --dev symfony/var-dumper
+composer require --dev symfony/maker-bundle
+
+# Require bundle env:PROD
+composer require symfony/security-bundle
+composer require symfony/rate-limiter
+composer require symfony/validator
+composer require symfony/serializer
+composer require symfony/serializer-pack
+composer require lexik/jwt-authentication-bundle
+composer require twig
+composer require asset
+composer require nelmio/cors-bundle
+composer require symfony/orm-pack:^2.1 -W
+composer require doctrine/doctrine-fixtures-bundle
+composer require fakerphp/faker
+
+# Creation keys JWT
+php bin/console lexik:jwt:generate-keypair

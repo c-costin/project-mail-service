367a0d11d6346019382dea4728bca707cacfd5c3
c-costin <contact@costincadeau.fr>
2024-02-09 17:00:47 +0100

    feat: create dashboard pages and navbar component

--- app/src/Controller/DashboardController.php
+++ app/src/Controller/DashboardController.php
@@ -11,8 +11,20 @@ class DashboardController extends AbstractController
     #[Route('/', name: 'app_default')]
     public function index(): Response
     {
-        return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
-        ]);
+        return $this->render('dashboard/index.html.twig',
+            []
+        );
+    }
+
+    #[Route('/applications', name: 'app_applications')]
+    public function applications(): Response
+    {
+        return $this->render('dashboard/applications.html.twig');
+    }
+
+    #[Route('/documentation', name: 'app_documentation')]
+    public function documentation(): Response
+    {
+        return $this->render('dashboard/documentation.html.twig');
     }
 }

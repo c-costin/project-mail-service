8c1a9fa175517ef9d704d085b8a40a62f2b676f7
c-costin <contact@costincadeau.fr>
2024-02-12 10:55:14 +0100

    feat: rework dashboard pages and links

---
+++ app/src/Controller/StaticsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StaticsController extends AbstractController
+{
+    #[Route('/statisques', name: 'app_statics')]
+    public function index(): Response
+    {
+        return $this->render('statics/index.html.twig');
+    }
+}

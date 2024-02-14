b420499e2b09bcf87bd81f3c08a0c482a438004d
c-costin <contact@costincadeau.fr>
2024-02-14 10:04:02 +0100

    feat: Adding CRUD for User informations

--- app/src/Controller/AccountController.php
+++ app/src/Controller/AccountController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,8 +17,23 @@ class AccountController extends AbstractController
         return $this->render(
             'account/index.html.twig',
             [
-                'currentPage' => 'account'
+                'currentPage' => 'account',
+                'user' => $this->getUser(),
             ]
         );
+    }
+
+    #[Route('/compte/suppression/{id}', name: 'app_account_remove', methods: ['POST'])]
+    public function remove(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        // if (!$project) {}
+
+        // Check is CSRF Token is valid
+        if ($this->isCsrfTokenValid('account_remove', $request->request->get('_csrf_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_logout');
+        }
     }
 }
